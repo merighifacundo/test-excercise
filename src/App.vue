1cb559900d581e814a19b05901bf95f3175fca6b
Facundo Merighi <merighifacundo@gmail.com>
2021-06-14 16:25:23 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <StatsFilterVue />
+</template>
+
+<script>
+import StatsFilterVue from "./components/StatsFilter.vue";
+export default {
+  name: "App",
+  components: {
+    StatsFilterVue: StatsFilterVue,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/StatsFilter.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="hello">
+    <h1>Simple Test</h1>
+    <p>This is a simple test Exercise</p>
+    <h3>Follow the instructions</h3>
+    <ul>
+      <li>
+        1. Add using reactive vue and state the number of stats that are in that
+        request
+      </li>
+      <li>
+        2. Create a simple table with the stats displaying only calories, and
+        distance
+      </li>
+      <li>3. Add a simple filter for the stats that are with calories > 900</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Test Exercise",
+  setup: () => {
+    const retrieveStats = async () => {
+      const response = await fetch("https://stats-tawny.vercel.app/api/stats");
+      const data = await response.json();
+      return data;
+    };
+
+    retrieveStats();
+
+    return {};
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
